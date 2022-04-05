dfc9b33eeede21847346afbbc6d6ecf624bddf10
Gavin <wugavin0573@gmail.com>
2022-04-04 20:06:20 -0700

    Modify imperativeprime.py to print statement when there are no prime numbers

--- repl/Week2/imperativeprime.py
+++ repl/Week2/imperativeprime.py
@@ -1,4 +1,5 @@
 def findprimes(a, b):
+    dne = True
     for i in range(a, b + 1):
         count = 0
         for g in range(2, (i // 2 + 1)):
@@ -6,8 +7,14 @@ def findprimes(a, b):
                 count = count + 1
                 break
         if (count == 0 and i != 1):
+            dne = False
+            # changes dne to False if there is a prime number is found
             print(" %d" %i, end=' ')
-    print()
+    if dne:
+        print("There are no prime numbers within this interval")
+        # prints if dne is True, meaning that there was no prime numbers found
+    else:
+        print()
 
 def primeTester():
     a = int(input("Enter the Minimum: "))
