6d1e879c96396f946b4bb3c4fbf49dafab8527c4
Gavin <wugavin0573@gmail.com>
2022-04-04 20:59:07 -0700

    Add quiz to menu 2

--- repl/menu.py
+++ repl/menu.py
@@ -8,9 +8,10 @@ from Week2 import factorial
 from Week2 import imperativeprime
 from Week2 import oopprime
 from Week2 import palindrome
+from Week3 import quiz
 
 main_menu = [
-    
+
 ]
 #week 0 submenu
 week0_list = [
@@ -21,25 +22,29 @@ week0_list = [
 ]
 #week 1 submenu
 week1_list = [
-  ["Fibonacci", fibonacci.fibonacciTester],
-  ["InfoDb(Just printing specific index)", Info.infoTester]
+    ["Fibonacci", fibonacci.fibonacciTester],
+    ["InfoDb(Just printing specific index)", Info.infoTester]
 ]
 
 #week 2 submenu
 week2_list = [
-  ["Factorial Class", factorial.factorialTester],
-  ["Prime Function Imperative", imperativeprime.primeTester],
-  ["Prime Function OOP", oopprime.primeOOPTester],
-  ["Palindrome Extra Credit", palindrome.palindromeTester]
+    ["Factorial Class", factorial.factorialTester],
+    ["Prime Function Imperative", imperativeprime.primeTester],
+    ["Prime Function OOP", oopprime.primeOOPTester],
+    ["Palindrome Extra Credit", palindrome.palindromeTester]
 ]
 
 # Second menu for iteration
 iteration_list = [
-  ["For Loop to print all of InfoDb", Info.forLoop],
-  ["While Loop to print all of InfoDb", Info.whileLoop ],
-  ["Recursive Loop to print all of InfoDb", Info.recursiveRun]
+    ["For Loop to print all of InfoDb", Info.forLoop],
+    ["While Loop to print all of InfoDb", Info.whileLoop ],
+    ["Recursive Loop to print all of InfoDb", Info.recursiveRun]
 ]
 
+#week 3 AKA cross over submenu
+week3_list = [
+    ["Quiz", quiz.starter],
+]
 
 border = "=" * 25
 banner = f"\n{border}\nPlease Select An Option\n{border}"
@@ -51,24 +56,29 @@ def menu():
     menu_list.append(["Week 0",week0Menu])
     menu_list.append(["Week 1",week1Menu])
     menu_list.append(["Week 2",week2Menu])
+    menu_list.append(["Week 3",week3Menu])
     buildMenu(title, menu_list)
-  
+
 def iterationMenu():
-  title = "Iteration Menu" + banner
-  buildMenu(title, iteration_list)
+    title = "Iteration Menu" + banner
+    buildMenu(title, iteration_list)
 
 def week0Menu():
-  title = "Week 0 Menu" + banner
-  buildMenu(title, week0_list)
-  
+    title = "Week 0 Menu" + banner
+    buildMenu(title, week0_list)
+
 def week1Menu():
-  title = "Week 1 Menu" + banner
-  week1_list.append(["Iteration to print Info", iterationMenu],)
-  buildMenu(title, week1_list)
+    title = "Week 1 Menu" + banner
+    week1_list.append(["Iteration to print Info", iterationMenu],)
+    buildMenu(title, week1_list)
 
 def week2Menu():
-  title = "Week 2 Menu" + banner
-  buildMenu(title, week2_list)
+    title = "Week 2 Menu" + banner
+    buildMenu(title, week2_list)
+
+def week3Menu():
+    title = "Week 2 Menu" + banner
+    buildMenu(title, week3_list)
 
 def buildMenu(banner, options):
 
@@ -95,7 +105,7 @@ def buildMenu(banner, options):
             action = prompts.get(choice)[1]
             action()
         except TypeError:
-            try:  
+            try:
                 exec(open(action).read())
             except FileNotFoundError:
                 print(f"File not found!: {action}")
@@ -106,7 +116,7 @@ def buildMenu(banner, options):
         print(f"Invalid choice: {choice}")
 
 
-    buildMenu(banner, options) 
+    buildMenu(banner, options)
 
 
 if __name__ == "__main__":
